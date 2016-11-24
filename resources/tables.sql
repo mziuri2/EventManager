cdee04b6c96090b53834d1af069477dcc384d249
tatopk <tatoleto12345@gmail.com>
2016-11-24 19:09:34 +0400

    added tables.sql

---
+++ resources/tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE EVENTS(
+    id INT PRIMARY KEY NOT NULL,
+    eventName VARCHAR(30) NOT NULL,
+    user_id INT NOT NULL,
+    rating INT NOT NULL,
+    place_id INT REFERENCES PLACES(id),
+    date DATE NOT NULL
+    )
+
+CREATE TABLE USERS(
+    id INT PRIMARY KEY NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    going_events LIST NOT NULL
+    );
+    
+CREATE TABLE PLACES(
+	id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    address VARCHAR(50) NOT NULL
+    );
+	
+CREATE TABLE GOINGEVENTS(
+	event_id INT REFERENCES EVENTS(id),
+	user_id INT REFERENCES USERS(id)
+);
