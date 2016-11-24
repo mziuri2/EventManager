832109a86d0929cc3ab1d972ff470f0c72afa4f5
gio123321 <gioleqo@gmail.com>
2016-11-24 16:36:00 +0300

    added getTodaysTop3Events()

--- ge/mziuri/eventmanager/dao/EventDAOImpl.java
+++ ge/mziuri/eventmanager/dao/EventDAOImpl.java
@@ -1,7 +1,9 @@
 package ge.mziuri.eventmanager.dao;
 
+import ge.mziuri.EventManager.metainfo.DatabaseMetaInfo;
 import ge.mziuri.eventmanager.model.Event;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,12 +17,24 @@ public class EventDAOImpl implements EventDAO{
     
     private PreparedStatement pstmt;
     
+    private int i=0;
+    
+    public EventDAOImpl() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(DatabaseMetaInfo.databaseURL, DatabaseMetaInfo.username, DatabaseMetaInfo.passsword);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
     @Override
     public void addEvent(Event event) {
         try {
-            pstmt = con.prepareStatement("INSERT INTO ");
+            pstmt = con.prepareStatement("INSERT INTO EVENTS (name, date, place_id) VALUES (?,?,?);");
             pstmt.setString(1, event.getEventName());
             pstmt.setDate(2, event.getDate());
+            pstmt.setInt(3,event.getPlace_id());
             pstmt.executeUpdate();
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
@@ -31,15 +45,16 @@ public class EventDAOImpl implements EventDAO{
     public List<Event> getAllEvents() {
         List<Event> events = new ArrayList<>();
         try {
-            pstmt = con.prepareStatement("SELECT * FROM ");
+            pstmt = con.prepareStatement("SELECT * FROM EVENTS");
             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
                 int id = rs.getInt("id");
-                String eventName = rs.getString("");
-                int user_id = rs.getInt("");
-                int rating = rs.getInt("");
-                int place_id = rs.getInt("");
-                Event event = new Event(id,eventName,user_id,rating,place_id);
+                String eventName = rs.getString("eventName");
+                int user_id = rs.getInt("user_id");
+                int rating = rs.getInt("rating");
+                int place_id = rs.getInt("place_id");
+                Date date = rs.getDate("date");
+                Event event = new Event(id,eventName,user_id,rating,place_id,date);
                 events.add(event);
             } 
         } catch (SQLException ex) {
@@ -50,6 +65,23 @@ public class EventDAOImpl implements EventDAO{
 
     @Override
     public List<Event> getTodaysTop3Events() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Event> events = new ArrayList<>();
+        try {
+            pstmt = con.prepareStatement("SELECT * FROM event ORDER BY rating ASC;");
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()&&i!=3) {
+                int id = rs.getInt("id");
+                String eventName = rs.getString("eventName");
+                int user_id = rs.getInt("user_id");
+                int rating = rs.getInt("rating");
+                int place_id = rs.getInt("place_id");
+                Date date = rs.getDate("date");
+                Event event = new Event(id,eventName,user_id,rating,place_id,date);
+                events.add(event);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return events;
     }  
 }
